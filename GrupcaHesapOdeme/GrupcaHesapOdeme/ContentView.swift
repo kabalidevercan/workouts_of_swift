5691f0341d6ec8a2c71f83d1ed47dcc076862dd5
kabalidevercan <kabalidevercan@gmail.com>
2024-02-05 11:44:13 +0300

    tipCalculator0.0.2

--- GrupcaHesapOdeme/GrupcaHesapOdeme/ContentView.swift
+++ GrupcaHesapOdeme/GrupcaHesapOdeme/ContentView.swift
@@ -8,25 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @FocusState private var amountIsFocused:Bool
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    let tipPercentages = [10,15,20,25,0]
+    var totalPerPerson:Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = (checkAmount / 100) * tipSelection
+        let totalValue = checkAmount + tipValue
+        let perValue = totalValue / peopleCount
+        return perValue
+    }
+    
+    
     var body: some View {
-        @State private var checkAmount = 0.0
-        @State private var numberOfPeople = 2
-        @State private var tipPercentage = 20
-        
-        let tipPercenages = [0,10,15,20,25]
-        
-        Form {
-            Section {
-                TextField("Amount",value: $checkAmount,format:.currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad)
-            }
-            Section {
-                Text(checkAmount,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount",value: $checkAmount,format:.currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number Of People:",selection: $numberOfPeople){
+                        ForEach(2..<100){value in
+                            Text("\(value)")
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                Section("Amount Per Person") {
+                    Text(totalPerPerson,format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                     
+                }
+                Section("How much tip do you want to leave? ") {
+                    Picker("Tip Percentage: ",selection: $tipPercentage){
+                        ForEach(tipPercentages,id: \.self){
+                            Text($0, format:.percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section {
+                    
+                }
             }
+                .navigationTitle("Alman Usulü")
+                .toolbar{
+                    if amountIsFocused {
+                        Button("Done"){
+                            amountIsFocused = false
+                        }
+                    }
+                }
+        }
+        
+            
         }
     }
-}
+
 
 #Preview {
     ContentView()
