b8da0930065a9c3488706f6b3d03b12d9f05ada0
kabalidevercan <kabalidevercan@gmail.com>
2024-02-05 23:59:27 +0300

    flagsqq

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Ercan Kabali on 5.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+
+    var body: some View {
+     Text("ss")
+    }
+}
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ercan Kabali on 5.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
