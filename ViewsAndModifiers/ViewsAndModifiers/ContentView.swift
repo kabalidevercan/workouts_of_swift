2d8c3d5aea160a0259cf8ac4adab51be4f55a9e0
kabalidevercan <kabalidevercan@gmail.com>
2024-02-07 23:57:10 +0300

    bazidenemeler

---
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  ViewsAndModifiers
+//
+//  Created by Ercan Kabali on 7.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+            
+        VStack {
+            Text("Hello, world!")
+                .padding()
+                .background(.red)
+                .padding()
+                .background(.blue)
+                .padding()
+                .background(.green)
+                .padding()
+                .background(.yellow)
+        }
+            
+           
+        
+        
+            
+      
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Ercan Kabali on 7.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
