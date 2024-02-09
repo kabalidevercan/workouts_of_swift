990df24b85cc875f4446842511698c5d786e1862
kabalidevercan <kabalidevercan@gmail.com>
2024-02-09 23:56:48 +0300

    BetterRest1.0.1

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         
@@ -29,6 +32,7 @@ struct ContentView: View {
                 Text("Daily Coffee Intake")
                     .font(.headline)
                 Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount,in: 1...20)
+                Spacer()
                     
             }
             .navigationTitle("BetterRest")
@@ -45,7 +49,19 @@ struct ContentView: View {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
             
+            let components = Calendar.current.dateComponents([.hour,.minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            
+            let prediction = try model.prediction(wake: Int64(Double(hour + minute)), estimatedSleep: sleepAmount, coffee: Int64(Double(coffeeAmount)))
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+                
         }catch {
+            alertTitle = "Your ideal bedtime is…"
+            
+            
             
         }
     }
