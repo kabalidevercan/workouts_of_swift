b191e86d56e1456aad6cfb75eba9a5114a9cb4e8
kabalidevercan <kabalidevercan@gmail.com>
2024-02-09 12:14:02 +0300

    BetterRest1.0.0

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -6,20 +6,50 @@
 //
 
 import SwiftUI
+import CoreML
+
 
 struct ContentView: View {
     
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
-    
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     
     var body: some View {
         
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount,in: 4...12,step: 0.25)
-        DatePicker("Please enter a date", selection: $wakeUp)
-        
+        NavigationStack {
+            VStack{
+                Text("When do you want to wake up ?")
+                    .font(.headline)
+                DatePicker("Please Enter a time", selection: $wakeUp,displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount,in: 4...12,step: 0.25)
+                Text("Daily Coffee Intake")
+                    .font(.headline)
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount,in: 1...20)
+                    
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate",action: calculateBedTime)
+            }
+            .padding()
+        }
         
     }
+    
+    func calculateBedTime(){
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+        }catch {
+            
+        }
+    }
+    
 }
 
 #Preview {
