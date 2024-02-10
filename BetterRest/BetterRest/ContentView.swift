0f73355f37ffb00b8a1f4cfe0f330fc847a0ab23
kabalidevercan <kabalidevercan@gmail.com>
2024-02-10 10:55:11 +0300

    BetterRest1.0.3

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -11,25 +11,33 @@ import CoreML
 
 struct ContentView: View {
     
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
+    static var defaultWakeTime:Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? .now
+    }
     
     var body: some View {
         
         NavigationStack {
             
-            VStack{
+            Form{
                 Text("When do you want to wake up ?")
                     .font(.headline)
                 DatePicker("Please Enter a time", selection: $wakeUp,displayedComponents: .hourAndMinute)
                     .labelsHidden()
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount,in: 4...12,step: 0.25)
+                VStack(alignment:.leading,spacing: 12){
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount,in: 4...12,step: 0.25)
+                }
                 Text("Daily Coffee Intake")
                     .font(.headline)
                 Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount,in: 1...20)
