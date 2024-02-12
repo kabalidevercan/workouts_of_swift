05c6f5ff374c226427affed1851915c18708f4e2
kabalidevercan <kabalidevercan@gmail.com>
2024-02-12 14:21:29 +0300

    WordScramble1.0.4

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord = ""
     @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+
     var body: some View {
         NavigationStack{
             List{
@@ -32,6 +36,11 @@ struct ContentView: View {
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
             .onAppear(perform:startGame)
+            .alert(errorTitle,isPresented: $showingError){
+                Button("Okay"){}
+            }message: {
+                Text(errorMessage)
+            }
         }
         
     }
@@ -40,6 +49,22 @@ struct ContentView: View {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
         guard answer.count > 0 else {return}
+        
+        guard isOriginal(word: answer)else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+        
+        guard isPossible(word: answer)else{
+            wordError(title: "Word not possible", message: "you cant spell")
+            return
+        }
+        
+        guard isReal(word: answer)else {
+            wordError(title: "Word not recognized", message: "You cant just make them up,you know!!")
+            return
+        }
+        
         
         withAnimation{
             usedWords.insert(answer, at: 0)
@@ -61,6 +86,44 @@ struct ContentView: View {
         fatalError("Couldnt load start.txt file from bundle!!!")
         
     }
+    
+    func isOriginal(word:String)-> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word:String) -> Bool {
+        var tempWord = rootWord
+        
+        
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter){
+                tempWord.remove(at: pos)
+            }else {
+                return false
+            }
+        }
+        
+        return true
+        
+    }
+    
+    func isReal(word:String)->Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title:String, message:String){
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    
+    
     
 }
 
