5c806437b948511541206973099a8fab6bc20940
kabalidevercan <kabalidevercan@gmail.com>
2024-02-17 00:46:36 +0300

    circleAnimation1

--- CircleAnimation/CircleAnimation/ContentView.swift
+++ CircleAnimation/CircleAnimation/ContentView.swift
@@ -11,34 +11,48 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
 
     var body: some View {
+        
         ZStack{
-            LinearGradient(colors: [.cyan,.yellow], startPoint: .top, endPoint: .bottom)
+            LinearGradient(colors: [.cyan,.black], startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
             VStack{
+           
+            
+            HStack{
+                Text("Get the premium version on KBLSoftware")
+                    .font(.system(size: 20))
+                    
                 Spacer()
-                Button("Tap Me") {
-                    // animationAmount += 1
-                }
-                .padding(35)
-                .background(.red)
-                .foregroundStyle(.white)
+                Image(systemName: "checkmark.circle.fill")
+                .padding(15)
+                .font(.footnote)
+                .background(.ultraThickMaterial)
+                .foregroundStyle(.red)
                 .clipShape(.circle)
                 .overlay(
                     Circle()
                         .stroke(.red)
                         .scaleEffect(animationAmount)
-                        .opacity(2 - animationAmount)
+                        .opacity(1.7 - animationAmount)
                         .animation(
                             .easeInOut(duration: 3)
-                                .repeatForever(autoreverses: false),
+                            .repeatForever(autoreverses: false),
                             value: animationAmount
                         )
                 )
                 .onAppear {
                     animationAmount = 2
                 }
-
+                
+                
             }
+            .padding()
+            .background(.ultraThickMaterial,in:RoundedRectangle(cornerRadius: 20 ,style: .continuous))
+            Spacer()
+            
+        }
+            
+            
         }
         
         
@@ -47,3 +61,38 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+/*
+ ZStack{
+     LinearGradient(colors: [.cyan,.yellow], startPoint: .top, endPoint: .bottom)
+         .ignoresSafeArea()
+     VStack{
+         Spacer()
+         Button("Tap Me") {
+             // animationAmount += 1
+         }
+         .padding(35)
+         .background(.red)
+         .foregroundStyle(.white)
+         .clipShape(.circle)
+         .overlay(
+             Circle()
+                 .stroke(.red)
+                 .scaleEffect(animationAmount)
+                 .opacity(2 - animationAmount)
+                 .animation(
+                     .easeInOut(duration: 3)
+                         .repeatForever(autoreverses: false),
+                     value: animationAmount
+                 )
+         )
+         .onAppear {
+             animationAmount = 2
+         }
+
+     }
+ }
+ */
+
+
