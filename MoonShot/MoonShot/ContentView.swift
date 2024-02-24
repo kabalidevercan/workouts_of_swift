c2a6e631babace1ae483d13268992863eb42699c
kabalidevercan <kabalidevercan@gmail.com>
2024-02-25 00:36:12 +0300

    MoonShot 0.0.2

--- MoonShot/MoonShot/ContentView.swift
+++ MoonShot/MoonShot/ContentView.swift
@@ -9,12 +9,57 @@ import SwiftUI
 
 struct ContentView : View{
     
-    let astronauts = Bundle.main.decode("astronauts.json")
+   let columns = [
+    GridItem(.adaptive(minimum: 150))
+   ]
+    
+    let astronauts : [String:Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
+
     
     
-   
     var body: some View{
-        Text(String(astronauts.count))
+        NavigationStack{
+            ScrollView{
+                LazyVGrid(columns:columns){
+                    ForEach(missions){mission in
+                        NavigationLink{
+                            Text("It is showing details right now!!")
+                        }label: {
+                            VStack{
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100,height: 100)
+                                    .padding()
+                                VStack{
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundStyle(.white)
+                                        
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.headline)
+                                        .foregroundStyle(.gray)
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth:.infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(RoundedRectangle(cornerRadius: 10).stroke(.lightBackground))
+                            
+                        }
+                    }
+                    .padding([.horizontal,.bottom])
+                }
+                
+            }
+            .navigationTitle("MoonShot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+        }
     }
 }
 
--- MoonShot/MoonShot/Mission.swift
+++ MoonShot/MoonShot/Mission.swift
@@ -14,10 +14,23 @@ struct Mission:Codable,Identifiable {
         let role:String
     }
     
-    let id:String
-    let launchDate:String?
+    let id: Int
+    let launchDate:Date?
     let crew:[CrewRole]
     let description:String
+    
+    
+    var displayName:String {
+        "Apollo \(id)"
+    }
+    
+    var image:String{
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate:String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
     
 }
 
