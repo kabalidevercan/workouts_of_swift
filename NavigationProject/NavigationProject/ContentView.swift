3d8fe00164fb8c5041fb879470b7799b5bfbee2c
kabalidevercan <kabalidevercan@gmail.com>
2024-03-01 16:46:08 +0300

    Navigation0.0.2

--- NavigationProject/NavigationProject/ContentView.swift
+++ NavigationProject/NavigationProject/ContentView.swift
@@ -24,10 +24,53 @@ struct User:Hashable{
 }
 
 
+struct  NewRandomView:View{
+    
+    var number:Int
+    @Binding var path :[Int]
+    
+    var body: some View{
+        NavigationLink("Go to random number",value: Int.random(in: 1...100))
+            .navigationTitle("Number: \(number)")
+        
+        if(!path.isEmpty){
+            Button("Back to home"){
+                path.removeAll()
+            }
+            .foregroundStyle(.white)
+            .frame(width: 200,height: 100)
+            .background(.linearGradient(colors: [.red.opacity(0.4),.blue.opacity(0.7)], startPoint: .bottomTrailing, endPoint: .topTrailing))
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .padding(.top,50)
+        }
+    }
+}
+
+
+
+struct ContentView:View{
+    
+    @State private var yol = [Int]()
+    
+    var body: some View{
+        NavigationStack(path:$yol){
+            NewRandomView(number: 59, path: $yol)
+                .navigationDestination(for: Int.self){IntValue in
+                    NewRandomView(number: IntValue, path: $yol)
+                }
+        }
+    }
+    
+}
+
+
+
+/*
+
 struct ContentView: View {
     
 
-    @State private var users = [User]()
+    @State private var users = NavigationPath()
     
     
     var body: some View {
@@ -43,7 +86,7 @@ struct ContentView: View {
                 .padding(.bottom,40)
                 
                 Button("Create a user for Ercan"){
-                    users = [User(name: "Ercan", surname: "Kabali", address: "Turkey")]
+                    users.append(User(name: "Ercan", surname: "Kabali", address: "Turkey"))
                 }
                 .foregroundStyle(.white)
                 .frame(width: 200,height: 100)
@@ -54,14 +97,35 @@ struct ContentView: View {
                     users.append(User(name: "Mehmet", surname: "Celi", address: "Turkey"))
                     users.append(User(name: "Zaara", surname: "Khan", address: "India"))
                 }
+                .foregroundStyle(.white)
+                .frame(width: 200,height: 100)
+                .background(.black)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .padding(.bottom,40)
+                ForEach(0..<5){i in
+                    Button("Create a number that is \(i)"){
+                        users.append(i)
+                    }
+                    .foregroundStyle(.white)
+                    .frame(width: 200,height: 100)
+                    .background(.black)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                }
+                .frame(maxWidth: .infinity)
             }
-            .frame(maxWidth: .infinity)
             .navigationDestination(for: User.self){selectedUser in
                 Text("You are able to see about \(selectedUser.name) \(selectedUser.surname),\(selectedUser.address)")
+            }
+            .navigationDestination(for: Int.self){createdInt in
+                    Text("You created a \(createdInt)")
             }
         }
     }
 }
+ 
+ */
+
         
 #Preview {
     ContentView()
---
+++ NavigationProject/NavigationProject/PathStore.swift
@@ -0,0 +1,121 @@
+//
+//  PathStore.swift
+//  NavigationProject
+//
+//  Created by Ercan Kabali on 1.03.2024.
+//
+
+import Foundation
+import SwiftUI
+
+/*
+@Observable
+class PathStore {
+    var path:  NavigationPath{
+        didSet {
+            save()
+        }
+    }
+
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+
+    
+
+    
+    
+    init() {
+        if let data = try? Data(contentsOf: savePath) {
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data) {
+                path = decoded
+                return
+            }
+        }
+
+        // Still here? Start with an empty path.
+        path = []
+    }
+
+    func save() {
+        do {
+            let data = try JSONEncoder().encode(path)
+            try data.write(to: savePath)
+        } catch {
+            print("Failed to save navigation data")
+        }
+    }
+}
+ */
+
+
+@Observable
+class StorePath{
+    var path:[Int]{
+        didSet{
+            save()
+        }
+    }
+    
+    private let savePath = URL.documentsDirectory.appending(path: "SavedPath")
+
+    init() {
+        if let data = try? Data(contentsOf: savePath){
+            if let decoded = try? JSONDecoder().decode([Int].self, from: data){
+                path = decoded
+                return
+            }
+        }
+        
+        path = []
+    }
+    
+    
+    func save(){
+        do{
+            let data = try JSONEncoder().encode(path)
+            try data.write(to: savePath )
+            
+            
+        }catch{
+            fatalError("Failed to save navigation Data")
+        }
+    }
+    
+}
+
+
+@Observable
+class PathStore{
+    
+    var path:NavigationPath{
+        didSet{
+            save()
+        }
+    }
+    
+    
+    let localPath = URL.documentsDirectory.appending(path: "SavedPath")
+    
+    init(){
+        if let data = try? Data(contentsOf: localPath){
+            if let decoded = try? JSONDecoder().decode(NavigationPath.CodableRepresentation.self, from: data){
+                path = NavigationPath(decoded)
+                return
+            }
+        }
+        
+        path = NavigationPath()
+        
+    }
+    
+    func save(){
+        do{
+            let data = try JSONEncoder().encode(path)
+            try data.write(to: localPath)
+            
+        }catch{
+            print("114.row in PathStore")
+        }
+    }
+    
+    
+}
