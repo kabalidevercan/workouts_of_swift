cdabefae5cee6a21ebf2743bbf4508ed47419110
kabalidevercan <kabalidevercan@gmail.com>
2024-03-21 01:37:43 +0300

    SwiftDataProject0.0.5

--- SwiftDataProject/SwiftDataProject/ContentView.swift
+++ SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -13,39 +13,57 @@ struct ContentView: View {
     @Environment(\.modelContext) var contextOfModel
     @State private var path = [User]()
     @State private var showingUpComingOnly = false
-    @Query(sort:\User.name) var users:[User]
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate)
+    ]
     
     
     var body: some View {
         NavigationStack(path: $path) {
-            List(users){user in
-                NavigationLink(value: user){
-                    Text(user.name)
+            UsersView(minimumJoinDate: showingUpComingOnly ? .now : .distantPast,sortOrder: sortOrder)
+                .navigationTitle("Users")
+                .navigationDestination(for: User.self) { user in
+                    EditUserView(user: user)
                 }
-            }
-            .navigationTitle("Users")
-            .navigationDestination(for: User.self) { user in
-                EditUserView(user: user)
-            }
-            .toolbar{
-                Button("Add Samples", systemImage: "plus") {
-                            
-                    try? contextOfModel.delete(model:User.self)
-                    
-                           let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
-                           let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
-                           let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
-                           let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
-
-                           contextOfModel.insert(first)
-                           contextOfModel.insert(second)
-                           contextOfModel.insert(third)
-                           contextOfModel.insert(fourth)
-                       }
-                Button(showingUpComingOnly ? "Show Everyone" : "Show Upcoming"){
-                    showingUpComingOnly.toggle()
+                .toolbar{
+                    //Button("Add random user"){
+                       /// let newUser = User(name: "", city: "", joinDate: .now)
+                       // contextOfModel.insert(newUser)
+                   //     //path = [newUser]
+                   // }
+                   // Button("Add Samples", systemImage: "plus") {
+                        
+                       // try? contextOfModel.delete(model:User.self)
+                       //
+                       // let first = User(name: "Ed Sheeran", city: "London", joinDate: .now.addingTimeInterval(86400 * -10))
+                       // let second = User(name: "Rosa Diaz", city: "New York", joinDate: .now.addingTimeInterval(86400 * -5))
+                       // let third = User(name: "Roy Kent", city: "London", joinDate: .now.addingTimeInterval(86400 * 5))
+                       // let fourth = User(name: "Johnny English", city: "London", joinDate: .now.addingTimeInterval(86400 * 10))
+                        
+                       // contextOfModel.insert(first)
+                       // contextOfModel.insert(second)
+                       // contextOfModel.insert(third)
+                      //  contextOfModel.insert(fourth)
+                 //   }
+                   // Button(showingUpComingOnly ? "Show Everyone" : "Show Upcoming"){
+                        //showingUpComingOnly.toggle()
+                    //}
+                    Menu("Sort",systemImage: "arrow.up.arrow.down"){
+                        Picker("Sort",selection: $sortOrder){
+                            Text("Sort by name")
+                                .tag([
+                                    SortDescriptor(\User.name),
+                                    SortDescriptor(\User.joinDate)
+                                ])
+                            Text("Sort by Join date")
+                                .tag([
+                                    SortDescriptor(\User.joinDate),
+                                    SortDescriptor(\User.name),
+                                ])
+                        }
+                    }
                 }
-            }
         }
     }
 }
--- SwiftDataProject/SwiftDataProject/UsersView.swift
+++ SwiftDataProject/SwiftDataProject/UsersView.swift
@@ -12,11 +12,12 @@ struct UsersView: View {
     @Query var users: [User]
 
 
-    init(minimumJoinDate: Date) {
+    init(minimumJoinDate: Date, sortOrder: [SortDescriptor<User>]) {
         _users = Query(filter: #Predicate<User> { user in
             user.joinDate >= minimumJoinDate
-        }, sort: \User.name)
+        }, sort: sortOrder)
     }
+    
     
     var body: some View {
         List(users) { user in
@@ -27,7 +28,7 @@ struct UsersView: View {
 
 #Preview {
 
-    UsersView(minimumJoinDate: .now)
+    UsersView(minimumJoinDate: .now,sortOrder: [SortDescriptor(\User.name)])
         .modelContainer(for: User.self)
     
 }
