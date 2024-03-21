4a397ead24ad5eed7369c52311194a59fc1ca355
kabalidevercan <kabalidevercan@gmail.com>
2024-03-21 23:58:26 +0300

    FilterImage0.1

---
+++ FilterImage/FilterImage/FilterImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterImageApp.swift
+//  FilterImage
+//
+//  Created by Ercan Kabali on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FilterImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
