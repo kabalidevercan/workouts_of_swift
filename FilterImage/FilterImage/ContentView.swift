5329f538e92afeb95ef108d6b364e3d280c1bdf1
kabalidevercan <kabalidevercan@gmail.com>
2024-03-26 07:31:13 +0300

    FilterImage0.2

--- FilterImage/FilterImage/ContentView.swift
+++ FilterImage/FilterImage/ContentView.swift
@@ -8,12 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amountOf = 0.0 {
+        didSet{
+            print("Worked\(amountOf)")
+        }
+    }
+    
+    @State private var watchingNewValue  = 0.0 {
+        didSet{
+            print("\(watchingNewValue) works fine")
+        }
+    }
+    
     var body: some View {
+        
+        VStack{
+            Text("New Text")
+                .blur(radius: watchingNewValue)
+            Slider(value: $watchingNewValue,in: 0...20)
+                .onChange(of: watchingNewValue){oldValue,newValue in
+                        print("old value is: \(oldValue)")
+                        print("new value is: \(newValue)")
+                }
+            
+            Button("Change the value",systemImage: "arrow.down.arrow.up"){
+                watchingNewValue = Double.random(in: 0...20)
+            }
+        }
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello Society")
+                .blur(radius: amountOf)
+            Slider(value: $amountOf,in: 0...20)
+            
+            Button("Random Blur"){
+                amountOf = Double.random(in: 0...20)
+            }
+            
+            
         }
         .padding()
     }
