e1af366c0975b8fa7937fb9bb92e4f01bf709c43
kabalidevercan <kabalidevercan@gmail.com>
2024-04-01 14:23:17 +0300

    FilterImage0.6

--- FilterImage/FilterImage/PhotosUITest.swift
+++ FilterImage/FilterImage/PhotosUITest.swift
@@ -11,14 +11,43 @@ import PhotosUI
 struct PhotosUITest: View {
     @State private var pickerItem : PhotosPickerItem?
     @State private var selectedImage: Image?
+    @State private var pickerItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
+    
+    
     
     var body: some View {
         VStack{
             PhotosPicker("Photo Picker", selection: $pickerItem,matching: .images)
+            selectedImage?
+                .resizable()
+                .scaledToFit()
         }
         .onChange(of:pickerItem){
             Task{
                 selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
+            }
+        }
+        VStack{
+            PhotosPicker("Select Images", selection: $pickerItems,matching: .images)
+        }
+        .onChange(of:pickerItems){
+            Task{
+                selectedImages.removeAll()
+                
+                for item in pickerItems {
+                    if let loadedImage = try await item.loadTransferable(type: Image.self){
+                        selectedImages.append(loadedImage)
+                    }
+                }
+            }
+        }
+        ScrollView{
+            ForEach(0..<selectedImages.count,id: \.self){
+                item in
+                selectedImages[item]
+                    .resizable()
+                    .scaledToFit()
             }
         }
     }
---
+++ FilterImage/FilterImage/SelectImageAndImagesTest.swift
@@ -0,0 +1,63 @@
+//
+//  SelectImageAndImagesTest.swift
+//  FilterImage
+//
+//  Created by Ercan Kabali on 1.04.2024.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct SelectImageAndImagesTest: View {
+    @State private var pickerItem : PhotosPickerItem?
+    @State private var selectedImage: Image?
+    @State private var listOfPickerItems = [PhotosPickerItem]()
+    @State private var listOfSelectedImages = [Image]()
+    
+    
+    var body: some View {
+        VStack{
+            PhotosPicker(selection: $listOfPickerItems,maxSelectionCount: 3,matching: .images){
+                Label("z;xlc;'z;'c.o", systemImage: "photo")
+            }
+            
+        }
+        .onChange(of: listOfPickerItems){
+            Task{
+                listOfSelectedImages.removeAll()
+                
+                for item in listOfPickerItems{
+                    if let loadedImage = try await item.loadTransferable(type: Image.self){
+                        listOfSelectedImages.append(loadedImage)
+                    }
+                }
+            }
+            
+        }
+
+        
+        ScrollView{
+            ForEach(0..<listOfSelectedImages.count,id: \.self){item in
+                    listOfSelectedImages[item]
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
+        
+       /* VStack{
+        PhotosPicker("Select One Image", selection: $pickerItem,matching:.images)
+        selectedImage?
+            .resizable()
+            .scaledToFit()
+    }
+    .onChange(of:pickerItem){
+        Task{
+            selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
+        }
+    }*/
+    }
+}
+
+#Preview {
+    SelectImageAndImagesTest()
+}
