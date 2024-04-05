44fdb12b6a7678465c1e0d7c68949b19eb5d2dce
kabalidevercan <kabalidevercan@gmail.com>
2024-04-05 12:03:44 +0300

    FilterImage1.0.3

--- FilterImage/FilterImage/ContentView.swift
+++ FilterImage/FilterImage/ContentView.swift
@@ -14,9 +14,29 @@ struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
     @State private var selectedItem : PhotosPickerItem?
+    @State private var currentFilter : CIFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    @State private var showingFilters = false
+    
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
+    }
+    
+    
+    func applyProcessing(){
+        currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        
+        guard let outputImage = currentFilter.outputImage else {return}
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else {return}
+        
+        let uIImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uIImage)
+    }
     
     func changeFilter(){
-        print("works great")
+        showingFilters = true
+
     }
     
     func loadImage(){
@@ -24,6 +44,10 @@ struct ContentView: View {
             guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else {return}
             guard let inputImage = UIImage(data: imageData) else {return}
             
+            
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
             /////more code to come
         }
     }
@@ -52,9 +76,15 @@ struct ContentView: View {
                         }
                     }
                 }
-                .onChange(of: selectedItem){
-                    loadImage() //BURDA DIGER TURLU DE YAZILABILIR EGER UYGULAMA CALISMAZSA BUNA DIKKAT ET !!!!!!!!!!!!!!!
-                }
+                .onChange(of: selectedItem,loadImage)
+                    
+                    
+                    //loadImage() //BURDA DIGER TURLU DE YAZILABILIR EGER UYGULAMA CALISMAZSA BUNA DIKKAT ET !!!!!!!!!!!!!!!
+                    
+                    /*
+                     .onChange(of:selectedItem,loadImage)
+                     */
+                
 
                 // image area
 
@@ -63,6 +93,7 @@ struct ContentView: View {
                 HStack {
                     Text("Intensity")
                     Slider(value: $filterIntensity)
+                        .onChange(of:filterIntensity,applyProcessing)
                 }
                 .padding(.vertical)
 
@@ -76,6 +107,19 @@ struct ContentView: View {
             }
             .padding([.horizontal, .bottom])
             .navigationTitle("Instafilter")
+            .confirmationDialog("Select a Filter", isPresented: $showingFilters){
+                
+                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
+                Button("Edges") { setFilter(CIFilter.edges()) }
+                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
+                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
+                Button("Vignette") { setFilter(CIFilter.vignette()) }
+                Button("Cancel", role: .cancel) { }
+                
+             //dialog here
+            }
         }
     }
 }
