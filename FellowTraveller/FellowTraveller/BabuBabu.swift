5340ecec65b84a81e509fd9a2466c8833baa9028
kabalidevercan <kabalidevercan@gmail.com>
2024-05-02 07:32:52 +0300

    FellowTraveller0.0.3

--- FellowTraveller/FellowTraveller/BabuBabu.swift
+++ FellowTraveller/FellowTraveller/BabuBabu.swift
@@ -6,8 +6,16 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct BabuBabu: View {
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
+            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
+        )
+    )
+    
     
     enum LoadingState{
         case loading,success,failed
@@ -16,6 +24,10 @@ struct BabuBabu: View {
     @State private var loadingState = LoadingState.loading
     
     var body: some View {
+       
+        Map(position: $position)
+        
+            
         if loadingState == .loading {
             LoadingView()
         } else if loadingState == .success {
