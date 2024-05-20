34fdb2e308ed402fe9dfb575f4584e470f7f2eef
kabalidevercan <kabalidevercan@gmail.com>
2024-05-20 17:11:05 +0300

    HotProspects0.0.1

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Ercan Kabali on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
